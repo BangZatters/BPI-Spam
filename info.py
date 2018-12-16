81f882eb72aa9961d4113f086251be02f455dd47
BangZatters <42405978+BangZatters@users.noreply.github.com>
2018-12-16 17:48:47 +0700

    Update info.py

--- info.py
+++ info.py
@@ -7,4 +7,4 @@ def mengetik(s):
         sys.stdout.write(c)
         sys.stdout.flush()
         time.sleep(random.random() * 0.2)
-mengetik(' \033[31;3m• B4n9Z4tt3r5<[BPI]\n \033[33;1m• 6etih666h05T<[BPI]\n \033[36;1m• 🐰InYourT3RS4K1T1_404<[BPI]🐰\n \033[37;1m• VIRxZero_<[BPI]')
+mengetik(' \033[31;3m• B4n9Z4tt3r5<[BPI]\n \033[33;1m• 6etih666h05T<[BPI]\n \033[36;1m• InYourT3R54K1T1_404<[BPI]\n \033[37;1m• VIRxZero_<[BPI]')
