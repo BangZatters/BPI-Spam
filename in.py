dee7948f17ad5524faaf7343f17c0282f8b2e5f8
BangZatters <42405978+BangZatters@users.noreply.github.com>
2018-12-17 16:04:43 +0700

    Update in.py

--- in.py
+++ in.py
@@ -6,4 +6,4 @@ def mengetik(s):
         sys.stdout.write(c)
         sys.stdout.flush()
         time.sleep(random.random() * 0.2)
-mengetik(' \033[31;1m• ✆••>€•LIT>MR.4Nz<[BPI] \n \033[33;1m• Mr.CL4Y0<[BPI] \n \033[37;1m• ᴹᴿ.$⁴ᴺᵀᴿ¹-SSC<[MCC]')
+mengetik(' \033[31;1m• ✆••>€•LIT>MR.4Nz<[BPI] \n \033[33;1m• Mr.CL4Y0<[BPI] \n \033[37;1m• ᴹᴿ.$⁴ᴺᵀᴿ¹-SSC<[BPI]<[MCC]')
